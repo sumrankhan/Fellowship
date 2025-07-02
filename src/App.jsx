2be84fd43274e3e58dcdcabcda241f07a2c50d78
pc-ITT <sumrankhan65@gmail.com>
2025-07-02 21:43:09 +0500

    commit project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Sumran from "./Sumran";
+function App() {
+  const lastname = "Khan";
+  //its a variable
+  return (
+    <>
+      <h1>
+        Hello Vite | Sumran {lastname}- learned about deleting unccessary files
+        and wrote H1 tag.
+      </h1>
+      <Sumran />
+    </>
+  );
+}
+
+export default App;
