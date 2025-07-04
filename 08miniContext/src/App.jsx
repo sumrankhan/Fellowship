e17b434f6b5d620b37980e9f67258c0ac55b126c
pc-ITT <sumrankhan65@gmail.com>
2025-07-04 15:42:53 +0500

    Context API project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./Context/UserContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Context API project </h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
--- currencyConvertor/src/hooks/useCurrencyInfo.js
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -4,13 +4,12 @@ function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json`
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json"
     )
-      .then((res) => res.json())
+      .then(() => resizeBy.json())
       .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
-  console.log(data);
   return data;
 }
 
