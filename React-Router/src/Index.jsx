dd6c08be080032acc999ba08a80cb5dc5718f265
pc-ITT <sumrankhan65@gmail.com>
2025-07-08 20:57:24 +0500

    remaining projects

---
+++ React-Router/src/Index.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+
+import React from "react";
+
+function Index() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default Index;
