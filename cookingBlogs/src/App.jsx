edc04ba48a0e63cd94b04283d8c52f173981ae90
pc-ITT <sumrankhan65@gmail.com>
2025-07-20 11:33:42 +0500

    Mega project

---
+++ cookingBlogs/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(process.env.REACT_APP - APPWRITE - URL);
+  return (
+    <>
+      <h1>Cooking blogs with Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
